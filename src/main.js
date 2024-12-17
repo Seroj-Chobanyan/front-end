d99a7e42d2ef30f42b4c617ea162ac16f7f16a9e
seroj-chobanyan <chobanyanserge@gmail.com>
2024-12-17 16:43:55 +0400

    Added new atems and

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css'
 import * as VueRouter from  'vue-router'
 import ShoppingCartPage from './pages/ShoppingCartPage.vue'
-import ProductDetailPage from './pages/ProductDetailPage.vue'
 import ProductsPages from './pages/ProductsPages.vue'
+import ProductDetailPage from './pages/ProductDetailPage.vue'
 
 createApp(App)
 .use(VueRouter.createRouter({
--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,9 +1,30 @@
 <template>
-    <h1>Shopping card page</h1>
-    </template>
-    
-    <script>
-    export default {
-        name: "ShoppingCardPage",
+    <h1>Shopping Cart</h1>
+    <div v-if="cartItems.length > 0">
+        <div class="product-container" v-for="product in cartItems" :key="product.id">
+            <img class="product-image" :src="product.imageName">
+            <div class="details-wrap">
+                <h3>{{ product.name }}</h3>
+                <p>{{ product.price }}</p>
+            </div>
+            <button class="remove-button button"> Remove from cart</button>
+        </div>
+        <button class="checkout-button button">Proced to Checkout</button>
+    </div>
+    <div v-if="cartItems.length === 0">
+        No item in you Cart
+    </div>
+</template>
+
+<script>
+import { cartItems } from '../temp-data';
+
+export default {
+    name: "ShoppingCartPage",
+    data() {
+        return {
+            cartItems,
+        }
     }
-    </script>
+}
+</script>
